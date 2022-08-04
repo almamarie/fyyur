1f26056575cbeceb53937525422c21b6c9ff3087
MarieLoumar <alouismariea97@gmail.com>
2022-08-04 12:39:30 +0000

    finished with listing artist

--- app.py
+++ app.py
@@ -268,66 +268,87 @@ def show_venue(venue_id):
         "facebook_link": venue.facebook_link,
         "seeking_talent": venue.seeking_talent,
         "image_link": venue.image_link,
-        "past_shows": [],
-        "upcoming_shows": [],
-        "past_shows_count": 0,
-        "upcoming_shows_count": 0,
     }
     print("venue: ", venue)
 
     # fetch all the past shows of the venue from the shows table
-    listOfPastShows = getListOfPastShows(venue_id)
+    listOfPastShows = getListOfPastShows("venue", venue_id)
 
     print("list of past shows", listOfPastShows)
     # update number of past shows
     template["past_shows_count"] = len(listOfPastShows)
 
     # for each show fetch the id (already gotten), name, and image of the artist performing in the show
-    template["past_shows"] = parseShows(listOfPastShows)
+    template["past_shows"] = parseShows("venue", listOfPastShows)
 
     # fetch all the upcoming shows of the venue
-    listOfUpcomingShows = getListOfUpcomingShows(venue_id)
+    listOfUpcomingShows = getListOfUpcomingShows("venue", venue_id)
 
     # update number of upcoming shows
     template["upcoming_shows_count"] = len(listOfUpcomingShows)
 
     # for each show fetch the id (already gotten), name, and image of the artist performing in the show
-    template["upcoming_shows"] = parseShows(listOfUpcomingShows)
+    template["upcoming_shows"] = parseShows("venue", listOfUpcomingShows)
 
     print('pastShows done: ', template)
     return render_template('pages/show_venue.html', venue=template)
 
 
-def parseShows(queryList):
+def parseShows(database, queryList):
     parsed = []
     print("parsing a list of ", len(queryList), " shows")
-    for pastShow in queryList:
-        print("pastshow", pastShow.id)
-        artistDetails = Artist.query.with_entities(
-            Artist.name, Artist.image_link).filter(Artist.id == pastShow.artist_id).first()
-        print(artistDetails)
-        tmp = {
-            "artist_id": pastShow.id,
-            "artist_name": artistDetails.name,
-            "artist_image_link": artistDetails.image_link,
-            "start_time": str(pastShow.start_time)
+    if database == "artist":
+        for pastShow in queryList:
+            print("pastshow", pastShow.id)
+            venueDetails = Artist.query.with_entities(
+                Artist.name, Artist.image_link).filter(Artist.id == pastShow.artist_id).first()
+            print(venueDetails)
+            tmp = {
+                "venue_id": pastShow.id,
+                "venue_name": venueDetails.name,
+                "venue_image_link": venueDetails.image_link,
+                "start_time": str(pastShow.start_time)
 
-        }
+            }
 
-        print("tmp: ", tmp)
-        parsed.append(tmp)
+            print("tmp: ", tmp)
+            parsed.append(tmp)
+    elif database == "venue":
+        for pastShow in queryList:
+            print("pastshow", pastShow.id)
+            artistDetails = Artist.query.with_entities(
+                Artist.name, Artist.image_link).filter(Artist.id == pastShow.artist_id).first()
+            print(artistDetails)
+            tmp = {
+                "artist_id": pastShow.id,
+                "artist_name": artistDetails.name,
+                "artist_image_link": artistDetails.image_link,
+                "start_time": str(pastShow.start_time)
 
+            }
+
+            print("tmp: ", tmp)
+            parsed.append(tmp)
     return parsed
 
 
-def getListOfPastShows(id):
-    return Show.query.filter(
-        Show.venue_id == id).filter(Show.start_time < datetime.now()).all()
+def getListOfPastShows(database, id):
+    if database == "venue":
+        return Show.query.filter(
+            Show.venue_id == id).filter(Show.start_time < datetime.now()).all()
+    elif database == "artist":
+        return Show.query.filter(
+            Show.artist_id == id).filter(Show.start_time < datetime.now()).all()
 
 
-def getListOfUpcomingShows(id):
-    return Show.query.filter(
-        Show.venue_id == id).filter(Show.start_time > datetime.now()).all()
+def getListOfUpcomingShows(database, id):
+    if database == "venue":
+        return Show.query.filter(
+            Show.venue_id == id).filter(Show.start_time > datetime.now()).all()
+    elif database == "artist":
+        return Show.query.filter(
+            Show.artist_id == id).filter(Show.start_time > datetime.now()).all()
+
 #  Create Venue
 #  ----------------------------------------------------------------
 
@@ -450,81 +471,52 @@ def search_artists():
 def show_artist(artist_id):
 
     # shows the artist page with the given artist_id
-    # TODO: replace with real artist data from the artist table, using artist_id
-    data1 = {
-        "id": 4,
-        "name": "Guns N Petals",
-        "genres": ["Rock n Roll"],
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "326-123-5000",
-        "website": "https://www.gunsnpetalsband.com",
-        "facebook_link": "https://www.facebook.com/GunsNPetals",
-        "seeking_venue": True,
-        "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-        "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-        "past_shows": [{
-            "venue_id": 1,
-            "venue_name": "The Musical Hop",
-            "venue_image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-            "start_time": "2019-05-21T21:30:00.000Z"
-        }],
-        "upcoming_shows": [],
-        "past_shows_count": 1,
-        "upcoming_shows_count": 0,
-    }
-    data2 = {
-        "id": 5,
-        "name": "Matt Quevedo",
-        "genres": ["Jazz"],
-        "city": "New York",
-        "state": "NY",
-        "phone": "300-400-5000",
-        "facebook_link": "https://www.facebook.com/mattquevedo923251523",
-        "seeking_venue": False,
-        "image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-        "past_shows": [{
-            "venue_id": 3,
-            "venue_name": "Park Square Live Music & Coffee",
-            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-            "start_time": "2019-06-15T23:00:00.000Z"
-        }],
-        "upcoming_shows": [],
-        "past_shows_count": 1,
-        "upcoming_shows_count": 0,
-    }
-    data3 = {
-        "id": 6,
-        "name": "The Wild Sax Band",
-        "genres": ["Jazz", "Classical"],
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "432-325-5432",
-        "seeking_venue": False,
-        "image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
+
+    # fetch the details of the artist with the id specified
+    artist = Artist.query.filter(Artist.id == artist_id).first()
+    # if not artist:
+    #     flash(
+    #         'An error occurred. artist ' + request.form['name'] + ' was not found.')
+    #     return
+
+    template = {
+        "id": artist_id,
+        "name": artist.name,
+        "genres": artist.genres,
+        "city": artist.city,
+        "state": artist.state,
+        "phone": artist.phone,
+        "seeking_venue": artist.seeking_venue,
+        "image_link": artist.image_link,
         "past_shows": [],
-        "upcoming_shows": [{
-            "venue_id": 3,
-            "venue_name": "Park Square Live Music & Coffee",
-            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-            "start_time": "2035-04-01T20:00:00.000Z"
-        }, {
-            "venue_id": 3,
-            "venue_name": "Park Square Live Music & Coffee",
-            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-            "start_time": "2035-04-08T20:00:00.000Z"
-        }, {
-            "venue_id": 3,
-            "venue_name": "Park Square Live Music & Coffee",
-            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-            "start_time": "2035-04-15T20:00:00.000Z"
-        }],
+        "upcoming_shows": [],
         "past_shows_count": 0,
-        "upcoming_shows_count": 3,
+        "upcoming_shows_count": 0,
     }
-    data = list(filter(lambda d: d['id'] ==
-                artist_id, [data1, data2, data3]))[0]
-    return render_template('pages/show_artist.html', artist=data)
+    print("artist: ", artist)
+
+    # fetch all the past shows of the artist from the shows table
+    listOfPastShows = getListOfPastShows("artist", artist_id)
+
+    print("list of past shows", listOfPastShows)
+    # update number of past shows
+    template["past_shows_count"] = len(listOfPastShows)
+
+    # for each show fetch the id (already gotten), name, and image of the artist performing in the show
+    template["past_shows"] = parseShows("artist", listOfPastShows)
+
+    # fetch all the upcoming shows of the artist
+    listOfUpcomingShows = getListOfUpcomingShows("artist", artist_id)
+
+    # update number of upcoming shows
+    template["upcoming_shows_count"] = len(listOfUpcomingShows)
+
+    # for each show fetch the id (already gotten), name, and image of the artist performing in the show
+    template["upcoming_shows"] = parseShows("artist", listOfUpcomingShows)
+
+    print('pastShows done: ', template)
+    return render_template('pages/show_artist.html', artist=template)
+
 
 #  Update
 #  ----------------------------------------------------------------
@@ -533,19 +525,7 @@ def show_artist(artist_id):
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
     form = ArtistForm()
-    artist = {
-        "id": 4,
-        "name": "Guns N Petals",
-        "genres": ["Rock n Roll"],
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "326-123-5000",
-        "website": "https://www.gunsnpetalsband.com",
-        "facebook_link": "https://www.facebook.com/GunsNPetals",
-        "seeking_venue": True,
-        "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-        "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-    }
+    artist = Artist.query.filter(Artist.id == artist_id).first()
     # TODO: populate form with fields from artist with ID <artist_id>
     return render_template('forms/edit_artist.html', form=form, artist=artist)
 
